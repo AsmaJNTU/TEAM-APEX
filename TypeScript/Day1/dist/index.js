2b92e54ceb7d7e9f5408efdb44da19865f1eb34d
Asma <asmabegum102004@gmail.com>
2024-11-23 00:05:23 +0530

    Day-1

---
+++ TypeScript/Day1/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits[1]);
+let nums = [1, 2, 3];
+nums.push(4);
+console.log(nums);
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/Day1/src/index.ts
@@ -0,0 +1,14 @@
+// let sales: number=454_665_876;
+// let course:string='TypeScript';
+// let is_published: boolean=true;
+
+//Arrays
+let fruits: string[] = ["Apple", "Banana", "Orange"];
+console.log(fruits[1]); // Output: Apple
+
+
+//Tuples
+let nums: number[] = [1, 2, 3];
+nums.push(4); // [1, 2, 3, 4]
+console.log(nums)
+
