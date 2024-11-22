51a1a40d45682397419b8f54c55bfe2c8725c5d7
Asma <asmabegum102004@gmail.com>
2024-11-23 00:39:29 +0530

    Day-1

--- TypeScript/Day1/dist/index.js
+++ TypeScript/Day1/dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 let fruits = ["Apple", "Banana", "Orange"];
-console.log(fruits[1]);
 let nums = [1, 2, 3];
 nums.push(4);
-console.log(nums);
+;
+let mySize = 3;
+function calculateTax(income, taxYear = 2024) {
+    if (taxYear < 2025)
+        return income * 1.2;
+    return income * 1.3;
+}
+console.log(calculateTax(10000));
 //# sourceMappingURL=index.js.map
