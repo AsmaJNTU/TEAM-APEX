23b0185f276174e57ec3605ec29bada82f32fe10
Asma <asmabegum102004@gmail.com>
2024-11-25 00:02:34 +0530

    Day-3

---
+++ TypeScript/Day-Work/src/Day3.ts
@@ -0,0 +1,21 @@
+//Variables
+
+let greetings:string="hello sir";
+
+console.log(greetings);
+
+//number
+let userId=77746.7
+userId.toFixed()
+
+// any
+
+let hero: string;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+export{}
