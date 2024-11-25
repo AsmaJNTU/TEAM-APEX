901db799cd7f1c62acd1d4aa9063a0ed8f95de33
Asma <asmabegum102004@gmail.com>
2024-11-25 19:16:01 +0530

    Day-3 added

---
+++ TypeScript/Day1/src/app.ts
@@ -0,0 +1,5 @@
+// console.log("welcome to my TypeScript");
+
+let age: number = 10;
+if(age <50)
+    age+=10;
---
+++ TypeScript/index.ts
@@ -0,0 +1,15 @@
+type Person={
+    name: string
+    age:number
+    isStudent: boolean
+}
+let Person1={
+    name: "asma",
+    age:19,
+    isStudent: true
+}
+let Person2:Person={
+    name: "V",
+    age:21,
+    isStudent: false
+}
