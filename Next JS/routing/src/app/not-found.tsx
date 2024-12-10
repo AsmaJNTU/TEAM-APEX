7fa820bceec8e4289b455df759263d6ba7aec359
Asma <asmabegum102004@gmail.com>
2024-12-10 22:56:46 +0530

    not-found page

---
+++ Next JS/routing/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div>
+      <h2>page not found</h2>
+      <p>Could not find requested resource 😊</p>
+    </div>
+  )
+}
+
+export default Notfound
