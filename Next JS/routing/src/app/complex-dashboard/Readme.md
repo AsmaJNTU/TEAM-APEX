6317a6dbe3afa272d589d1b93f9649f5dc3a20af
Asma <asmabegum102004@gmail.com>
2024-12-14 14:47:14 +0530

    Unmatched& Conditional Routes

---
+++ Next JS/routing/src/app/complex-dashboard/Readme.md
@@ -0,0 +1,8 @@
+Parallel Routes
+![alt text](<Screenshot 2024-12-14 144022.png>)
+
+Unmatched Routes
+![alt text](<Screenshot 2024-12-14 144337.png>)
+
+conditional routes
+![alt text](<Screenshot 2024-12-14 144103.png>)
--- Next JS/routing/src/app/complex-dashboard/layout.tsx
+++ Next JS/routing/src/app/complex-dashboard/layout.tsx
@@ -3,25 +3,28 @@ export default function DashboardLayout({
     users,
     revenue,
     notifications,
-    // login,
+     login,
   }: {
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
     notifications: React.ReactNode;
-    // login: React.ReactNode;
+    login: React.ReactNode;
   }) {
-    // const isLoggedIn = false;
-    return  (
+    const isLoggedIn = true;
+    return isLoggedIn ?(
       <div>
         <div>{children}</div>
         <div style={{ display: "flex" }}>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <div>{users}</div>
             <div>{revenue}</div>
+            <div>{login}</div>
           </div>
           <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
         </div>
       </div>
+    ):(
+      login
     ) 
   }
